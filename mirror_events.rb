fefdbe8ca713883ff2a275ee9c0bb2b56c8e5fa7
Georgios Gousios <gousiosg@gmail.com>
2012-01-26 16:50:55 +0200

    Exception handling, timestamp on output

--- mirror_events.rb
+++ mirror_events.rb
@@ -10,16 +10,22 @@ settings = YAML::load_file "config.yaml"
 github = GithubAnalysis.new
 
 EventMachine.run do
-  EventMachine.add_periodic_timer(1) do
-      events = github.api_request "https://api.github.com/events"
-      events.each do |e|
-        #next if e['type'] != "WatchEvent"
-        if github.events_col.find({'id' => e['id']}).has_next? then
-          puts "Already got #{e['id']}"
-        else
-          github.events_col.insert(e)
-          puts "Added #{e['id']}"
+  EventMachine.add_periodic_timer(5) do
+      begin
+        ts = Time.now().tv_sec()
+        events = github.api_request "https://api.github.com/events"
+
+        events.each do |e|
+          #next if e['type'] != "WatchEvent"
+          if github.events_col.find({'id' => e['id']}).has_next? then
+            puts "#{ts}: Already got #{e['id']}"
+          else
+            github.events_col.insert(e)
+            puts "#{ts}: Added #{e['id']}"
+          end
         end
+      rescue
+        # Ignore all exceptions, just retry next time
       end
   end
 end
