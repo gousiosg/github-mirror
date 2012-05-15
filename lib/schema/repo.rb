a601201155d7d7ca8efaa85494ed303739e09e52
Georgios Gousios <gousiosg@gmail.com>
2012-05-15 22:04:12 +0200

    Remove obsolete mongo-schema files

--- lib/schema/repo.rb
+++
@@ -1,4 +0,0 @@
-class Schema::Repo < Schema::SchemaBase
-
-
-end
--- lib/schema/schema_base.rb
+++
@@ -1,8 +0,0 @@
-class Schema::SchemaBase
-
-  attr_reader :timestamp
-
-    def initialize(timestamp = Time.now.to_i)
-      @timestamp = timestamp
-    end
-end
