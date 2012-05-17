9255efbbcd8004234959cd24eccbd2cf56e1a210
Georgios Gousios <gousiosg@gmail.com>
2012-05-18 00:51:44 +0200

    Make the db fields a bit more specific

--- lib/ghtorrent/migrations/003_add_external_ref_ids.rb
+++ lib/ghtorrent/migrations/003_add_external_ref_ids.rb
@@ -3,33 +3,33 @@ require 'sequel'
 Sequel.migration do
   up do
     alter_table :users do
-      add_column :ext_ref_id, :string, :null => false, :default => ""
+      add_column :ext_ref_id, String, :null => false, :size => 24, :default => "0"
     end
 
     alter_table :projects do
-          add_column :ext_ref_id, :string, :null => false, :default => ""
+      add_column :ext_ref_id, String, :null => false, :size => 24, :default => "0"
     end
 
     alter_table :commits do
-          add_column :ext_ref_id, :string, :null => false, :default => ""
+      add_column :ext_ref_id, String, :null => false, :size => 24, :default => "0"
     end
 
     alter_table :followers do
-          add_column :ext_ref_id, :string, :null => false, :default => ""
+      add_column :ext_ref_id, String, :null => false, :size => 24, :default => "0"
     end
   end
 
   down do
     alter_table :users do
-          drop_column :ext_ref_id
+      drop_column :ext_ref_id
     end
 
     alter_table :projects do
-          drop_column :ext_ref_id
+      drop_column :ext_ref_id
     end
 
     alter_table :commits do
-          drop_column :ext_ref_id
+      drop_column :ext_ref_id
     end
 
     alter_table :followers do
