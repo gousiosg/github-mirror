1d41a9f191b72cdc6a6570a695f6fd3f25b6f238
Georgios Gousios <gousiosg@gmail.com>
2012-07-16 13:05:08 +0200

    Correct foreign table. Add ext_ref_id field.

--- lib/ghtorrent/migrations/005_add_repo_collaborators.rb
+++ lib/ghtorrent/migrations/005_add_repo_collaborators.rb
@@ -6,10 +6,11 @@ Sequel.migration do
     puts("Adding table project members")
 
     create_table :project_members do
-      foreign_key :repo_id, :commits, :null => false
+      foreign_key :repo_id, :projects, :null => false
       foreign_key :user_id, :users, :null => false
       DateTime :created_at, :null => false,
                :default => Sequel::CURRENT_TIMESTAMP
+      String :ext_ref_id, :null => false, :size => 24, :default => "0"
       primary_key [:repo_id, :user_id]
     end
   end
