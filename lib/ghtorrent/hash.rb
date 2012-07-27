10ccc6e8cd922653187efc4cf0e5058bf2b51478
Georgios Gousios <gousiosg@gmail.com>
2012-07-27 10:53:58 +0200

    Method to recursively merge 2 hashes

---
+++ lib/ghtorrent/hash.rb
@@ -0,0 +1,11 @@
+class Hash
+  def merge_recursive(o, overwrite = true)
+    merge(o) do |_,x,y|
+      if x.respond_to?(:merge_recursive) && y.is_a?(Hash)
+        x.merge_recursive(y)
+      else
+        if overwrite then y else [*x,*y] end
+      end
+    end
+  end
+end
