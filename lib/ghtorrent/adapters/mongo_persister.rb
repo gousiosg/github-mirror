0792366e1ad7d17655d8c554deed6c83a48c9fb8
Georgios Gousios <gousiosg@gmail.com>
2012-08-03 16:23:55 +0200

    Lazy initialization of connection, collection and indexes

--- lib/ghtorrent/adapters/mongo_persister.rb
+++ lib/ghtorrent/adapters/mongo_persister.rb
@@ -28,53 +28,21 @@ module GHTorrent
 
       @settings = set
       @uniq = config(:uniq_id)
-
-      # Ensure that the necessary indexes exist
-      ensure_index(:events, "id")
-      ensure_index(:users, "login")
-      ensure_index(:commits, "sha")
-      ensure_index(:repos, "name")
-      ensure_index(:followers, "follows")
-      ensure_index(:org_members, "org")
-      ensure_index(:commit_comments, "repo")
-      ensure_index(:commit_comments, "user")
-      ensure_index(:commit_comments, "commit_id")
-      ensure_index(:repo_collaborators, "repo")
-      ensure_index(:repo_collaborators, "owner")
-      ensure_index(:repo_collaborators, "login")
-      ensure_index(:watchers, "repo")
-      ensure_index(:watchers, "owner")
-      ensure_index(:watchers, "login")
-      ensure_index(:pull_requests, "repo")
-      ensure_index(:pull_requests, "owner")
-      ensure_index(:forks, "repo")
-      ensure_index(:forks, "owner")
-      ensure_index(:forks, "id")
-
     end
 
     def store(entity, data = {})
       super
-      copy = get_entity(entity).insert(data).to_s
-      close
-      copy
+      get_entity(entity).insert(data).to_s
     end
 
     def find(entity, query = {})
       super
       result = get_entity(entity).find(query)
 
-      copy = result.to_a.map { |r|
+      result.to_a.map { |r|
         r[@uniq] = r['_id'].to_s;
         r.to_h
       }
-      close
-      copy.each {|x|
-        if x[@uniq].nil?
-          raise "#{@uniq} is null"
-        end
-      }
-      copy
     end
 
     # Find the record identified by +id+ in +entity+
@@ -86,9 +54,18 @@ module GHTorrent
     # Count the number of items returned by +query+
     def count(entity, query)
       super
-      copy = get_entity(entity).count(:query => query)
-      close
-      copy
+      get_entity(entity).count(:query => query)
+    end
+
+    def get_underlying_connection
+      mongo
+    end
+
+    def close
+      unless @mongo.nil?
+        @mongo.connection.close
+        @mongo = nil
+      end
     end
 
     private
@@ -98,50 +75,67 @@ module GHTorrent
     end
 
     def get_entity(entity)
-
-      col = case entity
-              when :users
-                get_collection("users")
-              when :commits
-                get_collection("commits")
-              when :repos
-                get_collection("repos")
-              when :followers
-                get_collection("followers")
-              when :org_members
-                get_collection("org_members")
-              when :events
-                get_collection("events")
-              when :commit_comments
-                get_collection("commit_comments")
-              when :repo_collaborators
-                get_collection("repo_collaborators")
-              when :watchers
-                get_collection("watchers")
-              when :pull_requests
-                get_collection("pull_requests")
-              when :forks
-                get_collection("forks")
-            end
-
-      if col.nil?
-        raise GHTorrentException.new("Mongo: Entity #{entity} not supported")
+      case entity
+        when :users
+          get_collection("users")
+        when :commits
+          get_collection("commits")
+        when :repos
+          get_collection("repos")
+        when :followers
+          get_collection("followers")
+        when :org_members
+          get_collection("org_members")
+        when :events
+          get_collection("events")
+        when :commit_comments
+          get_collection("commit_comments")
+        when :repo_collaborators
+          get_collection("repo_collaborators")
+        when :watchers
+          get_collection("watchers")
+        when :pull_requests
+          get_collection("pull_requests")
+        when :forks
+          get_collection("forks")
       end
-      col
     end
 
     def mongo
       @mongo ||= Mongo::Connection.new(config(:mongo_host),
                                      config(:mongo_port))\
-                                .db(config(:mongo_db))
-      @mongo
-    end
+                                  .db(config(:mongo_db))
 
-    def close
-      unless @mongo.nil?
-        @mongo.connection.close
-        @mongo = nil
+      if @mongo.collections.size <= 0
+
+        ENTITIES.each {|x|
+          get_entity(x)
+        }
+
+        # Ensure that the necessary indexes exist
+        ensure_index(:events, "id")
+        ensure_index(:users, "login")
+        ensure_index(:commits, "sha")
+        ensure_index(:repos, "name")
+        ensure_index(:followers, "follows")
+        ensure_index(:org_members, "org")
+        ensure_index(:commit_comments, "repo")
+        ensure_index(:commit_comments, "user")
+        ensure_index(:commit_comments, "commit_id")
+        ensure_index(:repo_collaborators, "repo")
+        ensure_index(:repo_collaborators, "owner")
+        ensure_index(:repo_collaborators, "login")
+        ensure_index(:watchers, "repo")
+        ensure_index(:watchers, "owner")
+        ensure_index(:watchers, "login")
+        ensure_index(:pull_requests, "repo")
+        ensure_index(:pull_requests, "owner")
+        ensure_index(:forks, "repo")
+        ensure_index(:forks, "owner")
+        ensure_index(:forks, "id")
       end
+
+      @mongo
     end
 
     # Declare an index on +field+ for +collection+ if it does not exist
