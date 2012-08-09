0375e3f555b533ca5f272e004d0c83b8e4074fa9
Georgios Gousios <gousiosg@gmail.com>
2012-08-09 14:21:19 +0200

    Take care of correctly deserializing arrays

--- lib/ghtorrent/bson_orderedhash.rb
+++ lib/ghtorrent/bson_orderedhash.rb
@@ -6,7 +6,9 @@ class BSON::OrderedHash
   def to_h
     inject({}) do |acc, element|
       k, v = element;
-      acc[k] = if v.class == BSON::OrderedHash then
+      acc[k] = if v.class == Array then
+                 v.map{|x| if x.class == BSON::OrderedHash then x.to_h else x end}
+               elsif v.class == BSON::OrderedHash then
                  v.to_h
                else
                  v
