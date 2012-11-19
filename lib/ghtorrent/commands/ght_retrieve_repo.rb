cd3fbfc82f5a318af52f25670f05fa95f0f55f31
Georgios Gousios <gousiosg@gmail.com>
2012-11-19 15:37:30 +0100

    Handle exceptions, don't start nested transactions

--- lib/ghtorrent/commands/ght_retrieve_repo.rb
+++ lib/ghtorrent/commands/ght_retrieve_repo.rb
@@ -79,13 +79,13 @@ end
 class TransactedGHTorrent < GHTorrent::Mirror
 
   def ensure_commit(repo, sha, user, comments = true)
-    transaction do
+    check_transaction do
       super(repo, sha, user, comments)
     end
   end
 
   def ensure_fork(owner, repo, fork_id, date_added = nil)
-    transaction do
+    check_transaction do
       super(owner, repo, fork_id, date_added)
     end
   end
@@ -93,26 +93,43 @@ class TransactedGHTorrent < GHTorrent::Mirror
   def ensure_pull_request(owner, repo, pullreq_id,
       comments = true, commits = true,
       state = nil, created_at = nil)
-    transaction do
+    check_transaction do
       super(owner, repo, pullreq_id, comments, commits, state, created_at)
     end
   end
 
   def ensure_issue(owner, repo, issue_id, events = true, comments = true)
-    transaction do
+    check_transaction do
       super(owner, repo, issue_id, events, comments)
     end
   end
 
   def ensure_project_member(owner, repo, new_member, date_added)
-    transaction do
+    check_transaction do
       super(owner, repo, new_member, date_added)
     end
   end
 
   def ensure_watcher(owner, repo, watcher, date_added = nil)
-    transaction do
+    check_transaction do
       super(owner, repo, watcher, date_added)
     end
   end
+
+  def check_transaction(&block)
+    begin
+      if @db.in_transaction?
+        debug "Transaction already started"
+        yield block
+      else
+        transaction do
+          yield block
+        end
+      end
+    rescue Exception => e
+      puts STDERR, e.message
+      puts STDERR, e.backtrace
+    end
+  end
+
 end
