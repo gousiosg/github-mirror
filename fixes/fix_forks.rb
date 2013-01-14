f18495b1450de9c3c110e651fc5d097d0c2c835c
Georgios Gousios <gousiosg@gmail.com>
2013-01-14 14:40:47 +0100

    Rename. Use 'parent' instead of 'source' to identify forked_from
    Thanks: @pengwynn

--- fixes/fix_forks.rb
+++ fixes/fix_forks.rb
@@ -28,38 +28,40 @@ class GHTFixForks < GHTorrent::Command
     @ght ||= GHTorrent::Mirror.new(settings)
     col = persister.get_underlying_connection.collection(:repos.to_s)
     fixed = tried = all = 0
-    col.find({"source" => {"$exists" => 1}}, {:timeout => false}) do |cursor|
+    col.find({"parent" => {"$exists" => 1}}, {:timeout => false}) do |cursor|
       cursor.each do |x|
         all += 1
         repo = x['name']
         owner = x['owner']['login']
-        source_owner = x['source']['owner']['login']
-        source_repo = x['source']['name']
+        parent_owner = x['parent']['owner']['login']
+        parent_repo = x['parent']['name']
 
         begin
           @ght.transaction do
             fork = @ght.ensure_repo(owner, repo, false, false, false)
 
-            source = @ght.ensure_repo(source_owner, source_repo, false, false, false)
+            parent = @ght.ensure_repo(parent_owner, parent_repo, false, false, false)
 
-            if source.nil?
-              puts("Source repo #{source_owner}/#{source_repo} does not exist")
+            if parent.nil?
+              puts("parent repo #{parent_owner}/#{parent_repo} does not exist")
               next
             end
 
             fork_exists = @ght.get_db[:forks].first(:forked_project_id => fork[:id],
-                                                    :forked_from_id => source[:id])
+                                                    :forked_from_id => parent[:id])
             if fork_exists.nil?
-              @ght.ensure_forks(source_owner, source_repo)
+              @ght.ensure_forks(parent_owner, parent_repo)
               tried += 1
               fork_exists = @ght.get_db[:forks].first(:forked_project_id => fork[:id],
-                                                      :forked_from_id => source[:id])
+                                                      :forked_from_id => parent[:id])
               if fork_exists.nil?
-                puts "Could not find fork #{owner}/#{repo} of #{source_owner}/#{source_repo}"
+                puts "Could not find fork #{owner}/#{repo} of #{parent_owner}/#{parent_repo}"
               else
-                puts "Added fork #{owner}/#{repo} of #{source_owner}/#{source_repo}"
+                puts "Added fork #{owner}/#{repo} of #{parent_owner}/#{parent_repo}"
                 fixed += 1
               end
+            else
+              puts "Fork #{owner}/#{repo} of #{parent_owner}/#{parent_repo} exists"
             end
             puts "Fixed #{fixed}/#{tried} (examined: #{all}) forks"
           end
