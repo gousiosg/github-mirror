079d6ac9e8496cf163c6bb4c922c5ae0a5a99b49
Georgios Gousios <gousiosg@gmail.com>
2013-02-11 09:56:58 +0100

    Correct query to update merged field

--- lib/ghtorrent/migrations/013_add_merged_to_pullreqs.rb
+++ lib/ghtorrent/migrations/013_add_merged_to_pullreqs.rb
@@ -15,8 +15,16 @@ Sequel.migration do
       set pr.merged = true
       where exists (select *
           from pull_request_commits prc, project_commits pc
-      where prc.commit_id = pc.commit_id
-      and prc.pull_request_id = pr.id)"
+          where prc.commit_id = pc.commit_id
+          and prc.pull_request_id = pr.id
+	        and pc.project_id = pr.base_repo_id
+          and pr.base_repo_id <> pr.head_repo_id)"
+      DB << "update pull_requests pr
+      set pr.merged = true
+      where exists(
+        select prh.created_at
+        from pull_request_history prh
+        where prh.action='merged' and prh.pull_request_id=pr.id)"
     end
 
     puts "Correcting intra_branch field"
