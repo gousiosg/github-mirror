861f6d65941b453f8592c31e234d58c8aa4a02d8
Georgios Gousios <gousiosg@gmail.com>
2013-03-13 13:21:21 +0200

    Support retrieving project to process through cmd-line

--- fixes/update_deleted.rb
+++ fixes/update_deleted.rb
@@ -8,6 +8,20 @@ class GHTFixDeleted < GHTorrent::Command
   include GHTorrent::Retriever
   include GHTorrent::Persister
 
+  def prepare_options(options)
+    options.banner <<-BANNER
+Updates the deleted field in the project table with current data
+
+#{command_name} owner repo
+
+    BANNER
+  end
+
+  def validate
+    super
+    Trollop::die "Either takes no arguments or two" if ARGV.size == 1
+  end
+
   def logger
     @ght.logger
   end
@@ -64,47 +78,53 @@ class GHTFixDeleted < GHTorrent::Command
     logger.debug("Project #{owner}/#{repo} updated")
   end
 
+  def process_project(owner, name)
+    @ght.transaction do
+
+      existing = persister.find(:repos, {'owner.login' => owner, 'name' => name })
+      on_github = api_request(ghurl ("repos/#{owner}/#{name}"))
+      retrieved = retrieve_repo(owner, name)
+
+      if existing.empty?
+        if on_github.empty?
+          # Project exists in MySQL but not on Github or Mongo
+          # Mark it as deleted
+          set_deleted(owner, name)
+        else
+          # Project does not exist in Mongo, but exists in MySQL and Github
+          # The retrieval process already added it to Mongo, so update MySQL
+          update_mysql(owner, name, retrieved)
+        end
+      else
+        if on_github.empty?
+          # Project was deleted on Github. Mark it as deleted.
+          set_deleted(owner, name)
+        else
+          update_mysql(owner, name, retrieved)
+        end
+      end
+    end
+  end
+
   def go
 
     @ght ||= GHTorrent::Mirror.new(settings)
 
-    projects = @ght.transaction do
+    unless ARGV[1].nil?
+      process_project(ARGV[0], ARGV[1])
+      exit(0)
+    end
+
+    @ght.transaction do
       a = db.from(:projects, :users).\
              where(:projects__owner_id => :users__id).\
              select(:users__login, :projects__name).\
-             limit(100).\
              all
-      a.map { |x| [x[:login],x[:name]] }
-    end
-
-    projects.each do |p|
-      @ght.transaction do
-        owner = p[0]
-        name = p[1]
-
-        existing = persister.find(:repos, {'owner.login' => owner, 'name' => name })
-        on_github = api_request(ghurl ("repos/#{owner}/#{name}"))
-        retrieved = retrieve_repo(owner, name)
-
-        if existing.empty?
-          if on_github.empty?
-            # Project exists in MySQL but not on Github or Mongo
-            # Mark it as deleted
-            set_deleted(owner, name)
-          else
-            # Project does not exist in Mongo, but exists in MySQL and Github
-            # The retrieval process already added it to Mongo, so update MySQL
-            update_mysql(owner, name, retrieved)
-          end
-        else
-          if on_github.empty?
-            # Project was deleted on Github. Mark it as deleted.
-            set_deleted(owner, name)
-          else
-            update_mysql(owner, name, retrieved)
-          end
-        end
-      end
+      a.map { |x| [x[:login], x[:name]] }
+    end.each do |p|
+      owner = p[0]
+      name = p[1]
+      process_project(owner, name)
     end
   end
 end
