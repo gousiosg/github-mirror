a3189ebc9bd5813deec7ba774d9e72fea1c01bfc
Georgios Gousios <gousiosg@gmail.com>
2013-09-14 12:40:13 +0200

    Fix migrations running on fresh DB

--- lib/ghtorrent/migrations/015_fix_table_issue_labels.rb
+++ lib/ghtorrent/migrations/015_fix_table_issue_labels.rb
@@ -6,12 +6,24 @@ Sequel.migration do
   up do
     puts "Fixing table issue_labels"
 
-    alter_table :issue_labels do
-      drop_column :repo_id
-      drop_column :ext_ref_id
-      add_foreign_key :issue_id, :issues
-      add_primary_key ([:issue_id, :label_id])
+    #alter_table :issue_labels do
+    #  drop_constraint 'issue_labels_ibfk_1'
+    #  drop_constraint 'issue_labels_ibfk_2'
+    #  drop_column :repo_id
+    #  drop_column :ext_ref_id
+    #  drop_column :label_id
+      #add_foreign_key :label_id, :repo_labels
+      #add_foreign_key :issue_id, :issues
+      #add_primary_key ([:issue_id, :label_id])
+    #end
+
+    drop_table :issue_labels
+    create_table :issue_labels do
+      foreign_key :label_id, :repo_labels
+      foreign_key :issue_id, :issues
+      primary_key [:issue_id, :label_id]
     end
+    
   end
 
   down do
--- lib/ghtorrent/migrations/016_add_actor_pull_request_history.rb
+++ lib/ghtorrent/migrations/016_add_actor_pull_request_history.rb
@@ -7,7 +7,7 @@ Sequel.migration do
     puts "Adding column user_id to table pull_rq"
 
     alter_table :pull_request_history do
-      add_foreign_key :actor_id, :user
+      add_foreign_key :actor_id, :users
     end
 
     puts 'Remember to run the fixes/update_pull_request_history_actor.rb
