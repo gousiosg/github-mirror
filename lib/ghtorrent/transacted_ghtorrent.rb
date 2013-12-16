fa89a9f7124937fea2df844af02414bc0037493a
Georgios Gousios <gousiosg@gmail.com>
2013-12-16 23:16:22 +0100

    More lower level transaction wrapped methods

--- lib/ghtorrent/transacted_ghtorrent.rb
+++ lib/ghtorrent/transacted_ghtorrent.rb
@@ -11,6 +11,12 @@ class TransactedGhtorrent < GHTorrent::Mirror
     end
   end
 
+  def ensure_commit_comment(owner, repo, sha, comment_id)
+    check_transaction do
+      super(owner, repo, sha, comment_id)
+    end
+  end
+
   def ensure_fork(owner, repo, fork_id)
     check_transaction do
       super(owner, repo, fork_id)
@@ -18,16 +24,40 @@ class TransactedGhtorrent < GHTorrent::Mirror
   end
 
   def ensure_pull_request(owner, repo, pullreq_id,
-      comments = false, commits = false, history = true,
+      comments = true, commits = true, history = true,
       state = nil, actor = nil, created_at = nil)
     check_transaction do
       super(owner, repo, pullreq_id, comments, commits, history, state, actor, created_at)
     end
   end
 
+  def ensure_pullreq_comment(owner, repo, pullreq_id, comment_id)
+    check_transaction do
+      super(owner, repo, pullreq_id, comment_id)
+    end
+  end
+
   def ensure_issue(owner, repo, issue_id, events = true, comments = true, labels = true)
     check_transaction do
       super(owner, repo, issue_id, events, comments, labels)
+    end
+  end
+
+  def ensure_issue_event(owner, repo, issue_id, event_id)
+    check_transaction do
+      super(owner, repo, issue_id, event_id)
+    end
+  end
+
+  def ensure_issue_comment(owner, repo, issue_id, comment_id, pull_req_id = nil)
+    check_transaction do
+      super(owner, repo, issue_id, comment_id, pull_req_id)
+    end
+  end
+
+  def ensure_issue_label(owner, repo, issue_id, name)
+    check_transaction do
+      super(owner, repo, issue_id, name)
     end
   end
 
