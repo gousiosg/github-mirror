a525bd128454055b79ede6da159123b43421bc3a
Georgios Gousios <gousiosg@gmail.com>
2014-02-04 10:06:18 +0100

    Convert to using the multiprocess queue

--- fixes/refresh_pull_request_history.rb
+++ fixes/refresh_pull_request_history.rb
@@ -2,12 +2,25 @@
 
 require 'ghtorrent'
 
-class GHTRefreshPullReqHistory < GHTorrent::Command
+class GHTRefreshPullReqHistory < MultiprocessQueueClient
+
+  def clazz
+    RefreshPullReqHistory
+  end
+
+end
+
+class RefreshPullReqHistory
 
   include GHTorrent::Settings
   include GHTorrent::Retriever
   include GHTorrent::Persister
   include GHTorrent::APIClient
+
+  def initialize(config, queue)
+    @config = config
+    @queue = queue
+  end
 
   def logger
     @ght.logger
@@ -23,44 +36,52 @@ class GHTRefreshPullReqHistory < GHTorrent::Command
     @ext_uniq
   end
 
+  def settings
+    @config
+  end
+
   def validate
     super
     Trollop::die 'Three arguments required' unless args[2] && !args[2].empty?
   end
 
-  def go
-    owner = ARGV[0]
-    repo = ARGV[1]
-    pull_req = ARGV[2]
+  def run(command)
 
-    @ght ||= GHTorrent::Mirror.new(settings)
-    col = persister.get_underlying_connection.collection(:pull_requests.to_s)
+    processor = Proc.new do |msg|
 
-    retrieved = api_request("https://api.github.com/repos/#{owner}/#{repo}/pulls/#{pull_req}")
+      owner, repo, pull_req = msg.split(/ /)
 
-    if retrieved.nil?
-      log.warn("Cannot retrieve #{owner}/#{repo} -> #{pull_req}")
-      return
-    end
+      @ght ||= GHTorrent::Mirror.new(settings)
+      col = persister.get_underlying_connection.collection(:pull_requests.to_s)
 
-    retrieved['owner'] = owner
-    retrieved['repo'] = repo
-    retrieved['number'] = pull_req.to_i
+      retrieved = api_request("https://api.github.com/repos/#{owner}/#{repo}/pulls/#{pull_req}")
 
-    col.remove({'owner' => owner, 'repo' => repo, 'number' => pull_req.to_i})
-    col.save(retrieved)
+      if retrieved.nil?
+        log.warn("Cannot retrieve #{owner}/#{repo} -> #{pull_req}")
+        return
+      end
 
-    @ght.get_db.from(:pull_request_history, :pull_requests,:users, :projects)\
-               .where(:pull_requests__id => :pull_request_history__pull_request_id)\
-               .where(:users__id => :projects__owner_id)\
-               .where(:projects__id => :pull_requests__base_repo_id)\
-               .where(:users__login => owner)\
-               .where(:projects__name => repo)\
-               .where(:pull_requests__pullreq_id => pull_req)\
-               .delete
+      retrieved['owner'] = owner
+      retrieved['repo'] = repo
+      retrieved['number'] = pull_req.to_i
 
-    @ght.ensure_pull_request(owner, repo, pull_req,
-                             comments = false, commits = false, history = true)
+      col.remove({'owner' => owner, 'repo' => repo, 'number' => pull_req.to_i})
+      col.save(retrieved)
+
+      @ght.get_db.from(:pull_request_history, :pull_requests, :users, :projects)\
+                 .where(:pull_requests__id => :pull_request_history__pull_request_id)\
+                 .where(:users__id => :projects__owner_id)\
+                 .where(:projects__id => :pull_requests__base_repo_id)\
+                 .where(:users__login => owner)\
+                 .where(:projects__name => repo)\
+                 .where(:pull_requests__pullreq_id => pull_req)\
+                 .delete
+
+      @ght.ensure_pull_request(owner, repo, pull_req,
+                               comments = false, commits = false, history = true)
+  end
+
+  command.queue_client(@queue, :after, processor)
   end
 
 end
--- lib/ghtorrent/commands/ght_retrieve_repos.rb
+++ lib/ghtorrent/commands/ght_retrieve_repos.rb
@@ -3,91 +3,15 @@ require 'ghtorrent/settings'
 require 'ghtorrent/logging'
 require 'ghtorrent/command'
 require 'ghtorrent/retriever'
+require 'ghtorrent/multiprocess_queue_client'
 require "bunny"
 
+class GHTRetrieveRepos < MultiprocessQueueClient
 
-class GHTRetrieveRepos < GHTorrent::Command
-
-  include GHTorrent::Settings
-  include GHTorrent::Logging
-
-  def logger
-    @logger ||= Logger.new(STDOUT)
-    @logger
+  def clazz
+    GHTRepoRetriever.class
   end
 
-  def prepare_options(options)
-    options.banner <<-BANNER
-Retrieve data for multiple repos in parallel. To work, it requires
-a mapping file formatted as follows:
-
-IP UNAME PASSWD NUM_PROCS where
-
-IP = address to use for outgoing requests (use 0.0.0.0 on non-multihomed hosts)
-UNAME = Github user name to use for outgoing requests
-PASSWD = Github password to use for outgoing requests
-NUM_PROCS = Number of processes to spawn for this IP/UNAME combination
-
-Values in the config.yaml file set with the -c command are overriden.
-
-#{command_name} [options] mapping-file
-
-    BANNER
-    options.opt :queue, 'Queue to retrieve project names from',
-                :short => 'q', :default => 'retrieve-repo', :type => :string
-
-  end
-
-  def validate
-    super
-    Trollop::die 'Argument mapping-file is required' unless not args[0].nil?
-  end
-
-  def go
-
-    configs = File.open(ARGV[0]).readlines.map do |line|
-      next if line =~ /^#/
-      ip,name,passwd,instances = line.strip.split(/ /)
-      (1..instances.to_i).map do |i|
-        newcfg = self.settings.clone
-        newcfg = override_config(newcfg, :attach_ip, ip)
-        newcfg = override_config(newcfg, :github_username, name)
-        newcfg = override_config(newcfg, :github_passwd, passwd)
-        newcfg = override_config(newcfg, :mirror_history_pages_back, 1000)
-        newcfg = override_config(newcfg, :mirror_commit_pages_new_repo, 1000)
-        newcfg
-      end
-    end.flatten.select{|x| !x.nil?}
-
-    children = configs.map do |config|
-      pid = Process::fork
-
-      if pid.nil?
-        retriever = GHTRepoRetriever.new(config, options[:queue])
-
-        Signal.trap('TERM') {
-          retriever.stop
-        }
-
-        retriever.run(self)
-        exit
-      else
-        debug "Parent #{Process.pid} forked child #{pid}"
-        pid
-      end
-    end
-
-    debug 'Waiting for children'
-    begin
-      children.each do |pid|
-        debug "Waiting for child #{pid}"
-        Process.waitpid(pid, 0)
-        debug "Child #{pid} exited"
-      end
-    rescue Interrupt
-      debug 'Stopping'
-    end
-  end
 end
 
 class GHTRepoRetriever
