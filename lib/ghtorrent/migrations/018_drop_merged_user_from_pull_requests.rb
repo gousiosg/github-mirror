c957a7c5d0341d63de3dfadd4e9279ae0b1a59a2
Georgios Gousios <gousiosg@gmail.com>
2014-03-27 13:20:00 +0100

    Correctly drop foreign key column

--- lib/ghtorrent/migrations/018_drop_merged_user_from_pull_requests.rb
+++ lib/ghtorrent/migrations/018_drop_merged_user_from_pull_requests.rb
@@ -3,16 +3,19 @@ require 'sequel'
 require 'ghtorrent/migrations/mysql_defaults'
 
 Sequel.migration do
+
   up do
     puts 'Dropping columns merged and user_id from pull_requests'
-    drop_column :pull_requests, :merged
-    drop_column :pull_requests, :user_id
+    alter_table :pull_requests do
+      drop_column :merged
+      drop_foreign_key :user_id
+    end
   end
 
   down do
     puts 'Adding columns merged and user_id to pull_requests'
     add_column :pull_requests, :merged, TrueClass, :null => false,
-               :default => false
+      :default => false
 
     add_foreign_key :user_id, :users, :null => false
 
