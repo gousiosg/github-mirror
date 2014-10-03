b9d116c5dba5dbe4a4977ec5476863ac69503d70
Georgios Gousios <gousiosg@gmail.com>
2014-10-03 12:05:29 +0200

    Only run MySQL code in MySQL database migrations

--- lib/ghtorrent/migrations/019_add_fake_to_users.rb
+++ lib/ghtorrent/migrations/019_add_fake_to_users.rb
@@ -2,22 +2,25 @@ require 'sequel'
 
 require 'ghtorrent/migrations/mysql_defaults'
 
+
 Sequel.migration do
 
   up do
     puts 'Add column fake to users'
       add_column :users, :fake, TrueClass, :null => false, :default => false
 
-    self.transaction(:rollback => :reraise, :isolation => :committed) do
-      self << "update users
-               set fake = '1'
-               where CAST(users.login AS BINARY) regexp '[A-Z]{8}'
-                and not exists (select * from pull_request_history where users.id = actor_id)
-                and not exists (select * from issue_events where actor_id = users.id)
-                and not exists (select * from project_members where users.id = user_id)
-                and not exists (select * from issues where reporter_id=users.id )
-                and not exists (select * from issues where assignee_id=users.id )
-                and not exists (select * from organization_members where user_id = users.id);"
+    if self.database_type == :mysql
+      self.transaction(:rollback => :reraise, :isolation => :committed) do
+        self << "update users
+                 set fake = '1'
+                 where CAST(users.login AS BINARY) regexp '[A-Z]{8}'
+                  and not exists (select * from pull_request_history where users.id = actor_id)
+                  and not exists (select * from issue_events where actor_id = users.id)
+                  and not exists (select * from project_members where users.id = user_id)
+                  and not exists (select * from issues where reporter_id=users.id )
+                  and not exists (select * from issues where assignee_id=users.id )
+                a nd not exists (select * from organization_members where user_id = users.id);"
+      end
     end
   end
 
