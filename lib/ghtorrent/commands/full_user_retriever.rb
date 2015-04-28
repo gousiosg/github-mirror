139684ee87821da13f5d7e19f880d47aa1c6521d
Georgios Gousios <gousiosg@gmail.com>
2015-04-28 13:21:17 +0200

    Move core functionality to shared module

---
+++ lib/ghtorrent/commands/full_user_retriever.rb
@@ -0,0 +1,50 @@
+module GHTorrent
+  module Commands
+    # Defines a process to download the full data available for a single user
+    module FullUserRetriever
+
+      def retrieve_user(login)
+        #self.settings = override_config(settings, :mirror_history_pages_back, -1)
+
+        user_entry = ght.transaction { ght.ensure_user(login, false, false) }
+        on_github = api_request(ghurl ("users/#{login}"))
+
+        if on_github.empty?
+          if user_entry.nil?
+            warn "User #{login} does not exist on GitHub"
+            exit
+          else
+            ght.transaction do
+              ght.get_db.from(:users).where(:login => login).update(:users__deleted => true)
+            end
+            warn "User #{user} marked as deleted"
+            return
+          end
+        else
+          if user_entry.nil?
+            warn "Error retrieving user #{login}"
+            exit
+          end
+        end
+
+        user = user_entry[:login]
+
+        def send_message(function, user)
+          begin
+            ght.send(function, user)
+          rescue Exception => e
+            puts STDERR, e.message
+            puts STDERR, e.backtrace
+          end
+        end
+
+        functions = %w(ensure_user_following ensure_user_followers ensure_orgs ensure_org)
+
+        functions.each do |x|
+          send_message(x, user)
+        end
+
+      end
+    end
+  end
+ end
--- lib/ghtorrent/commands/ght_retrieve_user.rb
+++ lib/ghtorrent/commands/ght_retrieve_user.rb
@@ -5,26 +5,20 @@ require 'ghtorrent/settings'
 require 'ghtorrent/command'
 require 'ghtorrent/retriever'
 require 'ghtorrent/transacted_gh_torrent'
-require 'ghtorrent/commands/ght_retrieve_repo'
+require 'ghtorrent/commands/full_user_retriever'
 
-class GHTRetrieveUser < GHTRetrieveRepo
+class GHTRetrieveUser < GHTorrent::Command
+
+  include GHTorrent::Retriever
+  include GHTorrent::Commands::FullUserRetriever
 
   def prepare_options(options)
     options.banner <<-BANNER
 An efficient way to get all data for a single user
 
-#{command_name} [options] user
+#{command_name} [options] login
 
     BANNER
-  end
-
-  def set_deleted(user)
-    ght.transaction do
-      ght.get_db.from(:users).\
-       where(:login => user).\
-       update(:users__deleted => true)
-    end
-    warn "User #{user} marked as deleted"
   end
 
   def validate
@@ -32,70 +26,14 @@ An efficient way to get all data for a single user
     Trollop::die "One argument is required" unless args[0] && !args[0].empty?
   end
 
+  def ght
+    @ght ||= TransactedGHTorrent.new(settings)
+    @ght
+  end
+
   def go
-    self.settings = override_config(settings, :mirror_history_pages_back, -1)
-
-    user_entry = ght.transaction{ght.ensure_user(ARGV[0], false, false)}
-    on_github = api_request(ghurl ("users/#{ARGV[0]}"))
-
-    if on_github.empty?
-      if user_entry.nil?
-        warn "User #{ARGV[0]} does not exist on GitHub"
-        exit
-      else
-        set_deleted(ARGV[0])
-        exit
-      end
-    else
-      if user_entry.nil?
-        warn "Error retrieving user #{ARGV[0]}"
-        exit
-      end
-    end
-
-    user = user_entry[:login]
-
-    def send_message(function, user)
-      begin
-        ght.send(function, user)
-      rescue Exception => e
-        puts STDERR, e.message
-        puts STDERR, e.backtrace
-      end
-    end
-
-    functions = %w(ensure_user_following ensure_user_followers ensure_orgs ensure_org)
-
-    if ARGV[1].nil?
-      functions.each do |x|
-        send_message(x, user)
-      end
-    else
-      Trollop::die("Not a valid function: #{ARGV[1]}") unless functions.include? ARGV[1]
-      send_message(ARGV[1], user)
-    end
-
-  end
-end
-
-class TransactedGHTorrent
-
-  def ensure_user_followers(user)
-    check_transaction do
-      super(user)
-    end
-  end
-
-  def ensure_orgs(user)
-    check_transaction do
-      super(user)
-    end
-  end
-
-  def ensure_org(user, members = true)
-    check_transaction do
-      super(user, members)
-    end
+    login = ARGV[0]
+    retrieve_user(login)
   end
 
 end
