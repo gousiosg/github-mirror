1fb77f9b15ad8155a9ada1c6478a57073d5eebc6
Georgios Gousios <gousiosg@gmail.com>
2015-06-09 10:44:17 +0200

    Configuration options for custom retrieval stages

--- lib/ghtorrent/commands/ght_retrieve_repo.rb
+++ lib/ghtorrent/commands/ght_retrieve_repo.rb
@@ -10,6 +10,7 @@ class GHTRetrieveRepo < GHTorrent::Command
   include GHTorrent::Settings
   include GHTorrent::Retriever
   include GHTorrent::Persister
+  include GHTorrent::EventProcessing
 
   def prepare_options(options)
     options.banner <<-BANNER
@@ -18,12 +19,36 @@ An efficient way to get all data for a single repo
 #{command_name} [options] owner repo
 
     BANNER
+    options.opt :no_events, 'Skip retrieving events', :default => false
+    options.opt :no_entities, 'Skip retrieving entities', :default => false
+
+    options.opt :only_stage, "Only do the provided stage of entity retrieval (one of: #{stages.join(',')})",
+                :type => String
+    options.opt :exclude_events, 'Comma separated list of event types to exclude from processing',
+                :type => String
   end
 
   def validate
     super
-    Trollop::die "Two arguments are required" unless args[0] && !args[0].empty?
+    Trollop::die 'Two arguments are required' unless args[0] && !args[0].empty?
+
+    unless options[:exclude_events].nil?
+      @exclude_event_types = options[:exclude_events].split(/,/)
+    else
+      @exclude_event_types = []
+    end
+
+    unless options[:only_stage].nil?
+      Trollop::die("Not a valid function: #{options[:only_stage]}") unless stages.include? options[:only_stage]
+    end
+
   end
+
+  def stages
+    %w(ensure_commits ensure_forks ensure_pull_requests
+       ensure_issues ensure_watchers ensure_labels) #ensure_project_members
+  end
+
 
   def persister
     @persister ||= connect(:mongo, settings)
@@ -35,14 +60,14 @@ An efficient way to get all data for a single repo
     @ext_uniq
   end
 
-  def ght
-    @ght ||= TransactedGHTorrent.new(settings)
-    @ght
+  def ghtorrent
+    @ghtorrent ||= TransactedGHTorrent.new(settings)
+    @ghtorrent
   end
 
   def go
     self.settings = override_config(settings, :mirror_history_pages_back, 1000)
-    user_entry = ght.transaction{ght.ensure_user(ARGV[0], false, false)}
+    user_entry = ghtorrent.transaction{ghtorrent.ensure_user(ARGV[0], false, false)}
 
     if user_entry.nil?
       Trollop::die "Cannot find user #{ARGV[0]}"
@@ -50,7 +75,7 @@ An efficient way to get all data for a single repo
 
     user = user_entry[:login]
 
-    repo_entry = ght.transaction{ght.ensure_repo(ARGV[0], ARGV[1])}
+    repo_entry = ghtorrent.transaction{ghtorrent.ensure_repo(ARGV[0], ARGV[1])}
 
     if repo_entry.nil?
       Trollop::die "Cannot find repository #{ARGV[0]}/#{ARGV[1]}"
@@ -58,21 +83,28 @@ An efficient way to get all data for a single repo
 
     repo = repo_entry[:name]
 
-    def send_message(function, user, repo)
-      ght.send(function, user, repo)
-    end
-
-    functions = %w(ensure_commits ensure_forks ensure_pull_requests
-       ensure_issues ensure_watchers ensure_labels) #ensure_project_members
-
-    if ARGV[2].nil?
-      functions.each do |x|
-        send_message(x, user, repo)
+    unless options[:no_entities_given]
+      if options[:only_stage].nil?
+        stages.each do |x|
+          ghtorrent.send(x, user, repo)
+        end
+      else
+        ghtorrent.send(options[:only_stage], user, repo)
       end
-    else
-      Trollop::die("Not a valid function: #{ARGV[2]}") unless functions.include? ARGV[2]
-      send_message(ARGV[2], user, repo)
     end
+
+    # Process repo events
+    unless options[:no_events_given]
+      get_repo_events(ARGV[0], ARGV[1]).each do |event|
+        begin
+          unless @exclude_event_types.include? event['type']
+            send(event['type'], event)
+          end
+        rescue Exception => e
+           puts "Could not process event #{event['type']}-#{event['id']}: #{e.message}"
+        end
+      end
+    end
+
   end
 end
-
