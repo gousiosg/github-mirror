89554d45b1656dabe217879130d6ea8440691e3f
Georgios Gousios <gousiosg@gmail.com>
2015-10-08 11:17:06 +0200

    Fix migration

--- lib/ghtorrent/migrations/023_geolocate_users.rb
+++ lib/ghtorrent/migrations/023_geolocate_users.rb
@@ -7,11 +7,11 @@ Sequel.migration do
   up do
     puts 'Adding geo-location fields to table users'
     alter_table :users do
-      add_column :long, BigDecimal, :size=>[11,8], :default => :null
-      add_column :lat, BigDecimal, :size=>[10,8], :default => :null
-      add_column :country_code, String, :fixed => :true, :size => 3, :default => :null
-      add_column :state, :location, :default => :null
-      add_column :city, :location, :default => :null
+      add_column :long, BigDecimal, :size=>[11,8]
+      add_column :lat, BigDecimal, :size=>[10,8]
+      add_column :country_code, String, :fixed => :true, :size => 3
+      add_column :state, String
+      add_column :city, String
     end
   end
 
