f64d5979e69e0fe641cb6485f8552ea4c12ad938
Georgios Gousios <gousiosg@gmail.com>
2016-01-06 11:24:26 +0100

    Missing indexes and fixes in dump script

--- sql/indexes.sql
+++ sql/indexes.sql
@@ -1,5 +1,7 @@
-CREATE UNIQUE INDEX `login` ON `ghtorrent`.`users` (`login` ASC)  COMMENT '';
-CREATE UNIQUE INDEX `sha` ON `ghtorrent`.`commits` (`sha` ASC)  COMMENT '';
-CREATE UNIQUE INDEX `comment_id` ON `ghtorrent`.`commit_comments` (`comment_id` ASC)  COMMENT '';
+CREATE UNIQUE INDEX IF NOT EXISTS `login` ON `ghtorrent`.`users` (`login` ASC)  COMMENT '';
+CREATE UNIQUE INDEX  IF NOT EXISTS  `sha` ON `ghtorrent`.`commits` (`sha` ASC)  COMMENT '';
+CREATE UNIQUE INDEX  IF NOT EXISTS  `comment_id` ON `ghtorrent`.`commit_comments` (`comment_id` ASC)  COMMENT '';
 CREATE INDEX `follower_id` ON `ghtorrent`.`followers` (`follower_id` ASC) COMMENT '';
-CREATE UNIQUE INDEX `pullreq_id` ON `ghtorrent`.`pull_requests` (`pullreq_id` ASC, `base_repo_id` ASC)  COMMENT '';
+CREATE UNIQUE INDEX  IF NOT EXISTS  `pullreq_id` ON `ghtorrent`.`pull_requests` (`pullreq_id` ASC, `base_repo_id` ASC)  COMMENT '';
+CREATE INDEX  IF NOT EXISTS  `name` ON `ghtorrent`.`projects` (`name` ASC)  COMMENT '';
+CREATE INDEX `commit_id` ON `ghtorrent`.`project_commits` (`commit_id` ASC)  COMMENT '';
