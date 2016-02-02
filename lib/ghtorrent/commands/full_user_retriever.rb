bb29c230a02701c11e1709eb90289e422a8e28f5
Georgios Gousios <gousiosg@gmail.com>
2016-02-02 13:43:37 +0100

    Update MongoDB when updating users from GitHub
    Also, update all MySQL user entries to
    new values retrieved from GitHub

--- lib/ghtorrent/commands/full_user_retriever.rb
+++ lib/ghtorrent/commands/full_user_retriever.rb
@@ -18,6 +18,19 @@ module GHTorrent
         @ghtorrent
       end
 
+      def update_mongo(login, new_user)
+        r = persister.\
+            get_underlying_connection[:users].\
+            remove({'login' => login})
+        persister.\
+            get_underlying_connection[:users].\
+            insert(new_user)
+        if r['n'] > 0
+          debug("MongoDB entry for user #{login} updated (#{r['n']} records removed)")
+        else
+          debug("Added MongoDB entry for user #{login}")
+        end
+      end
 
       def retrieve_user(login)
         debug "User #{login} update started"
@@ -42,19 +55,29 @@ module GHTorrent
           end
         end
 
+        # Refresh MongoDb with the latest info from GitHub
+        unless on_github.empty?
+          update_mongo(login, on_github)
+        end
+
         # Update geo location information
         geo = geolocate(on_github['location'])
  
         ght.get_db.from(:users).where(:login => login).update(
-          :users__long => geo['long'].to_f,
-          :users__lat => geo['lat'].to_f,
+          # Geolocation info
+          :users__long         => geo['long'].to_f,
+          :users__lat          => geo['lat'].to_f,
           :users__country_code => geo['country_code'],
-          :users__state => geo['state'],
-          :users__city => geo['city'],
-        )
+          :users__state        => geo['state'],
+          :users__city         => geo['city'],
+          :users__location     => on_github['location'],
 
-        ght.get_db.from(:users).where(:login => login).update(
-          :users__location => on_github['location']
+          # user details
+          :users__name         => on_github['name'],
+          :users__company      => on_github['company'],
+          :users__email        => on_github['email'],
+          :users__deleted      => false,
+          :users__fake         => false
         )
 
         user = user_entry[:login]
