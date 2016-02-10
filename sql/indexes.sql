14d0e3fe2e9ac4ebde6a2aa1eb99104bfb5f32f8
Sebastian Baltes <s.baltes@uni-trier.de>
2016-02-10 21:29:40 +0100

    Added create statement for missing index for column project_id of table project_languages.

--- sql/indexes.sql
+++ sql/indexes.sql
@@ -5,3 +5,4 @@ CREATE INDEX `follower_id` ON `ghtorrent`.`followers` (`follower_id` ASC) COMMEN
 CREATE UNIQUE INDEX  IF NOT EXISTS  `pullreq_id` ON `ghtorrent`.`pull_requests` (`pullreq_id` ASC, `base_repo_id` ASC)  COMMENT '';
 CREATE INDEX  IF NOT EXISTS  `name` ON `ghtorrent`.`projects` (`name` ASC)  COMMENT '';
 CREATE INDEX `commit_id` ON `ghtorrent`.`project_commits` (`commit_id` ASC)  COMMENT '';
+CREATE INDEX `project_id` ON ghtorrent`.`project_languages` (`project_id`) COMMENT '';
