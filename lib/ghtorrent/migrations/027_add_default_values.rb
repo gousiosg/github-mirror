0ddd7718544cb577289eb67482b35257135e433e
Georgios Gousios <gousiosg@gmail.com>
2016-04-18 14:01:42 +0200

    Fake entries for users and repos
    This is to ensure that when we retrieve
    entities (repos, issues etc) whose parents have
    gone away, we have a way to indicate the fact
    that they have gone away.

---
+++ lib/ghtorrent/migrations/027_add_default_values.rb
@@ -0,0 +1,28 @@
+require 'sequel'
+
+require 'ghtorrent/migrations/mysql_defaults'
+
+Sequel.migration do
+
+  up do
+    puts 'Adding fake user and project entries '
+    self[:users].insert(
+      :id => -1,
+      :login => '0xnoone',
+      :email => 'no@nowhere.noplace',
+      :name => 'Fake entry to indicate a missing user previously existing')
+
+    self[:projects].insert(
+        :id => -1,
+        :name => 'noproject',
+        :owner_id => -1,
+        :description => 'Fake entry to indicate a previously existing but currently absent repo')
+  end
+
+  down do
+    puts 'Dropping fake user and project entries '
+    DB[:projects].where(:id => -1).delete
+    DB[:users].where(:id => -1).delete
+  end
+
+end
