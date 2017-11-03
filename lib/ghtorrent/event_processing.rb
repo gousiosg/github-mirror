7982fa72f54eeb3fde6dfc5e3726a76cc4853dff
Andrew Colello <acolello@blackducksoftware.com>
2017-11-03 15:31:21 -0400

    fix sql bug

--- lib/ghtorrent/event_processing.rb
+++ lib/ghtorrent/event_processing.rb
@@ -44,9 +44,9 @@ module GHTorrent
             next if push_commits.include? url[7]
 
             sha_not_exist = ght.db.from(:commits, :project_commits, :projects, :users).\
-                            where(Sequel.qualify('projects', 'id') => Sequel.qualify('project_commits', 'project_id)')).\
-                            where(Sequel.qualify('commits', 'id') => Sequel.qualify('project_commits', 'commit_id)')).\
-                            where(Sequel.qualify('projects', 'owner_id') => Sequel.qualify('users', 'id)')).\
+                            where(Sequel.qualify('projects', 'id') => Sequel.qualify('project_commits', 'project_id')).\
+                            where(Sequel.qualify('commits', 'id') => Sequel.qualify('project_commits', 'commit_id')).\
+                            where(Sequel.qualify('projects', 'owner_id') => Sequel.qualify('users', 'id')).\
                             where(Sequel.qualify('projects', 'name') => url[5]).\
                             where(Sequel.qualify('users', 'login') => url[4]).\
                             where(Sequel.qualify('commits', 'sha') => url[7]).all.empty?
