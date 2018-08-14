9aa75c410cd69759f12a26650084b0a82eb638c7
Priya Rani <prani@blackducksoftware.com>
2018-08-14 08:32:23 +0530

    Updated the dump/restore scripts in the sql directory

--- sql/README.md
+++ sql/README.md
@@ -1,67 +1,62 @@
 ## Restoring GHTorrent backups
 
-The GHTorrent MySQL comes in CSV files, one per database table. This is
-to avoid expensive FK checks and index creation that comes by default
-in the MySQL dump process. To restore the database as a whole,
+The GHTorrent PostgreSQL comes in compressed CSV files, one per database table. This is
+to avoid expensive FK checks and index creation. To restore the database as a whole,
 do the following:
 
-**WARNING**: The following process will delete an existing database if you
+**WARNING**: The following process will delete tables of an existing database if you
 use the same database name (or if you use the default database name).
 
-### Download the MySQL dump
+### Download the PostgreSQL dump
 
 You can find all downloads at the [GHTorrent downloads page](http://ghtorrent.org/downloads.html). 
-To download a dump, replace `mysql-yyyy-mm-dd` in the code snippets below
+To download a dump, replace `pg-yyyy-mm-dd` in the code snippets below
 with one of the available downloads on that page.
 
 ```bash
-wget http://ghtorrent.org/downloads/mysql-yyyy-mm-dd.tar.gz
-tar zxvf mysql-yyyy-mm-dd.tar.gz
+wget http://ghtorrent.org/downloads/pg-yyyy-mm-dd.tar.gz
+tar zxvf pg-yyyy-mm-dd.tar.gz
 ```
 
 If you have a reliable internet connection you can avoid the overhead of
 storing the compressed file.
 
 ```bash
-curl http://ghtorrent.org/downloads/mysql-yyyy-mm-dd.tar.gz |
+curl http://ghtorrent.org/downloads/pg-yyyy-mm-dd.tar.gz |
 tar zxvf -
 ```
 
-### Create a MySQL user
-Create a MySQL user with permissions to create new schemata, for example:
+### Create a PostgreSQL user
+Create a PostgreSQL user with permissions to create new schemata, for example:
 
 ```sql
-create user ghtorrentuser@'localhost' identified by 'ghtorrentpassword';
-create user ghtorrentuser@'*' identified by 'ghtorrentpassword';
-create database ghtorrent_restore;
-grant all privileges on ghtorrent_restore.* to 'ghtorrentuser'@'localhost';
-grant all privileges on ghtorrent_restore.* to 'ghtorrentuser'@'*';
-grant file on *.* to 'ghtorrentuser'@'localhost';
+CREATE DATABASE ghtorrent_restore;
+CREATE USER ghtorrentuser WITH PASSWORD 'ghtorrentpassword';
+GRANT ALL PRIVILEGES ON DATABASE "ghtorrent_restore" to ghtorrentuser;
 ```
 ### Run the restore process
 
-The run the `ght-restore-mysql` script like this (replace the `ghtorrentuser`
+The run the `ght-restore-pg` script like this (replace the `ghtorrentuser`
 and `ghtorrentpassword` with the actual values you specified above):
 
 ```bash
-cd  mysql-yyyy-mm-dd
-./ght-restore-mysql -u ghtorrentuser -d ghtorrent_restore -p ghtorrentpassword .
+cd  pg-yyyy-mm-dd
+./ght-restore-pg -u ghtorrentuser -d ghtorrent_restore -p ghtorrentpassword .
 ```
 
 ### Restoring individual tables
 If you want to restore CSV files individually, you first need to create
-the schema using the 'schema.sql' file. Then, login to the MySQL command
+the schema using the 'schema.sql' file. Then, login to the PostgreSQL command
 promtp and do the following (the example will just load the `users` table):
 
 ```sql
-mysql> SET foreign_key_checks = 0;
-mysql> LOAD DATA INFILE '/full/path/to/users.csv' INTO TABLE users FIELDS TERMINATED BY ',' OPTIONALLY ENCLOSED BY '\"' LINES TERMINATED BY '\n'
+ghtorrent_restore=# COPY users FROM PROGRAM 'zcat < /full/path/to/users.csv.gz';
 ```
 
 The `ORDER` file defines the order the CSV files should be imported, if you want
 to avoid FK missing conflicts.
 
-You can then create the corresponding indexes from the `indexes.sql` file.
+You can then create the corresponding indexes and foreign keys from the `indexes_and_foreign_keys` file.
 
 ### Restoring user private data
 
@@ -73,8 +68,9 @@ To restore the user private data in the `users` table, run the following
 commands:
 
 ```bash
-gunzip users-private-yyyy-mm-dd.gz
+mv users-private-yyyy-mm-dd.csv.gz users_private.csv.gz
 wget https://raw.githubusercontent.com/gousiosg/github-mirror/master/sql/ght-add-private
+wget https://raw.githubusercontent.com/gousiosg/github-mirror/master/sql/users_private.sql
 chmod +x ght-add-private
 ./ght-add-private -u ghtorrentuser -d ghtorrent_restore -p ghtorrentpassword .
 ```
--- sql/users_private.sql
+++ sql/users_private.sql
@@ -1,42 +1,43 @@
+-- Table: users_private
 DROP TABLE IF EXISTS users_private;
 
 CREATE TABLE users_private (
-  login VARCHAR(255) NOT NULL,
-  name VARCHAR(255) NOT NULL,
-  email VARCHAR(255) NOT NULL,
-  PRIMARY KEY (login))
-ENGINE = InnoDB
-DEFAULT CHARACTER SET = utf8;
+  login character varying(255) NOT NULL,
+  name character varying(255),
+  email character varying(255)
+);
+ALTER TABLE ONLY users_private ADD CONSTRAINT users_private_pkey PRIMARY KEY (login);
 
-SET foreign_key_checks = 0;
-LOAD DATA INFILE 'USERS_PRIVATE_FILE' INTO TABLE users_private
-CHARACTER SET UTF8 FIELDS TERMINATED BY ','
-OPTIONALLY ENCLOSED BY '"'
-LINES TERMINATED BY '\n';
+COPY users_private FROM PROGRAM 'zcat < USERS_PRIVATE_FILE';
 
-CREATE UNIQUE INDEX login ON users_private (login ASC);
+CREATE UNIQUE INDEX users_private_login ON users_private (login ASC);
+
+-- Table: users_new
+DROP SEQUENCE IF EXISTS users_new_id_seq CASCADE;
+CREATE SEQUENCE users_new_id_seq INCREMENT BY 1
+                                  NO MAXVALUE NO MINVALUE CACHE 1;
+SELECT pg_catalog.setval('users_new_id_seq', 1, true);
 
 DROP TABLE IF EXISTS users_new;
 
 CREATE TABLE users_new (
-  id INT(11) NOT NULL AUTO_INCREMENT,
-  login VARCHAR(255) NOT NULL,
-  name VARCHAR(255) NOT NULL,
-  company VARCHAR(255) NULL DEFAULT NULL,
-  location VARCHAR(255) NULL DEFAULT NULL,
-  email VARCHAR(255) NOT NULL,
-  created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  type VARCHAR(255) NOT NULL DEFAULT 'USR',
-  fake TINYINT(1) NOT NULL DEFAULT '0',
-  deleted TINYINT(1) NOT NULL DEFAULT '0',
-  `long` DECIMAL(11,8),
-  lat DECIMAL(10,8),
-  country_code CHAR(3),
-  state VARCHAR(255),
-  city VARCHAR(255),
-  PRIMARY KEY (id) )
-ENGINE = InnoDB
-DEFAULT CHARACTER SET = utf8;
+  id integer DEFAULT nextval('users_new_id_seq'::regclass) NOT NULL,
+  login character varying(255) NOT NULL,
+  name character varying(255),
+  company character varying(255),
+  email character varying(255),
+  created_at timestamp without time zone DEFAULT CURRENT_TIMESTAMP NOT NULL,
+  type character varying(255) NOT NULL DEFAULT 'USR',
+  fake boolean DEFAULT false NOT NULL,
+  deleted boolean DEFAULT false NOT NULL,
+  long numeric(11, 8),
+  lat numeric(10, 8),
+  country_code character(3),
+  state character varying(255),
+  city character varying(255),
+  location character varying(255) DEFAULT NULL
+);
+ALTER TABLE ONLY users_new ADD CONSTRAINT users_new_pkey PRIMARY KEY (id);
 
 INSERT INTO users_new (
   SELECT
@@ -44,26 +45,73 @@ INSERT INTO users_new (
     users.login as login,
     users_private.name as name,
     users.company as company,
-    users.location as location,
-    users_private.email as email,
+    users_private.email as email,    
     users.created_at as created_at,
     users.type as type,
     users.fake as fake,
     users.deleted as deleted,
-    users.`long` as `long`,
+    users.long as long,
     users.lat as lat,
     users.country_code as country_code,
     users.state as state,
-    users.city as city
+    users.city as city,
+    users.location as location
   FROM users LEFT JOIN users_private ON
   users.login = users_private.login
 );
 
-CREATE UNIQUE INDEX login ON users_new (login ASC);
+CREATE UNIQUE INDEX users_new_login ON users_new (login ASC);
 
-RENAME TABLE users TO users_old;
+ALTER TABLE users RENAME TO users_old;
+ALTER TABLE users_new RENAME TO users;
 
-RENAME TABLE users_new TO users;
+ALTER TABLE "commit_comments" DROP CONSTRAINT commit_comments_user_id_fkey;
+ALTER TABLE "commit_comments" ADD FOREIGN KEY ("user_id") REFERENCES "users"(id);
+
+ALTER TABLE "commits" DROP CONSTRAINT commits_author_id_fkey;
+ALTER TABLE "commits" ADD FOREIGN KEY ("author_id") REFERENCES "users"(id);
+
+ALTER TABLE "commits" DROP CONSTRAINT commits_committer_id_fkey;
+ALTER TABLE "commits" ADD FOREIGN KEY ("committer_id") REFERENCES "users"(id);
+
+ALTER TABLE "followers" DROP CONSTRAINT followers_follower_id_fkey;
+ALTER TABLE "followers" ADD FOREIGN KEY ("follower_id") REFERENCES "users"(id);
+
+ALTER TABLE "followers" DROP CONSTRAINT followers_user_id_fkey;
+ALTER TABLE "followers" ADD FOREIGN KEY ("user_id") REFERENCES "users"(id);
+
+ALTER TABLE "issue_comments" DROP CONSTRAINT issue_comments_user_id_fkey;
+ALTER TABLE "issue_comments" ADD FOREIGN KEY ("user_id") REFERENCES "users"(id);
+
+ALTER TABLE "issue_events" DROP CONSTRAINT issue_events_actor_id_fkey;
+ALTER TABLE "issue_events" ADD FOREIGN KEY ("actor_id") REFERENCES "users"(id);
+
+ALTER TABLE "issues" DROP CONSTRAINT issues_reporter_id_fkey;
+ALTER TABLE "issues" ADD FOREIGN KEY ("reporter_id") REFERENCES "users"(id);
+
+ALTER TABLE "issues" DROP CONSTRAINT issues_assignee_id_fkey;
+ALTER TABLE "issues" ADD FOREIGN KEY ("assignee_id") REFERENCES "users"(id);
+
+ALTER TABLE "organization_members" DROP CONSTRAINT organization_members_org_id_fkey;
+ALTER TABLE "organization_members" ADD FOREIGN KEY ("org_id") REFERENCES "users"(id);
+
+ALTER TABLE "organization_members" DROP CONSTRAINT organization_members_user_id_fkey;
+ALTER TABLE "organization_members" ADD FOREIGN KEY ("user_id") REFERENCES "users"(id);
+
+ALTER TABLE "project_members" DROP CONSTRAINT project_members_user_id_fkey;
+ALTER TABLE "project_members" ADD FOREIGN KEY ("user_id") REFERENCES "users"(id);
+
+ALTER TABLE "projects" DROP CONSTRAINT projects_owner_id_fkey;
+ALTER TABLE "projects" ADD FOREIGN KEY ("owner_id") REFERENCES "users"(id);
+
+ALTER TABLE "pull_request_comments" DROP CONSTRAINT pull_request_comments_user_id_fkey;
+ALTER TABLE "pull_request_comments" ADD FOREIGN KEY ("user_id") REFERENCES "users"(id);
+
+ALTER TABLE "pull_request_history" DROP CONSTRAINT pull_request_history_actor_id_fkey;
+ALTER TABLE "pull_request_history" ADD FOREIGN KEY ("actor_id") REFERENCES "users"(id);
+
+ALTER TABLE "watchers" DROP CONSTRAINT watchers_user_id_fkey;
+ALTER TABLE "watchers" ADD FOREIGN KEY ("user_id") REFERENCES "users"(id);
 
 DROP TABLE users_old;
 DROP TABLE users_private;
