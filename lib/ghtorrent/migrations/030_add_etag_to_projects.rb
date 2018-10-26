0b70a25e4b65f7849c52a03de90fbe73a02bc41d
Georgios Gousios <gousiosg@gmail.com>
2018-10-26 23:36:04 +0200

    Add an etag field to projects
    ref: #74

---
+++ lib/ghtorrent/migrations/030_add_etag_to_projects.rb
@@ -0,0 +1,19 @@
+require 'sequel'
+require 'ghtorrent/migrations/mysql_defaults'
+
+
+Sequel.migration do
+
+  up do
+    puts 'Adding column etag to table projects'
+    add_column :projects, :etag, String,
+               :null => true, :size => 40
+  end
+
+  down do
+    puts 'Deleting column updated_at from table projects'
+    alter_table :projects do
+      drop_column :etag
+    end
+  end
+end
