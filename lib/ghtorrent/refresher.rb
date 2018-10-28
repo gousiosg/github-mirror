de50a50abe1c73d04b7e62612e8e6ce47b15d089
Georgios Gousios <gousiosg@gmail.com>
2018-10-28 22:20:53 +0000

    Fix update logic to avoid too freq updates

--- lib/ghtorrent/refresher.rb
+++ lib/ghtorrent/refresher.rb
@@ -7,23 +7,24 @@ module GHTorrent
 
     def refresh_repo(owner, repo, db_entry)
 
-      return db_entry if Time.now - db_entry[:updated_at] < 3600 * 24
+      now = Time.now
+      return db_entry if now.to_i - db_entry[:updated_at].to_i < 3600 * 24
 
       etag = db_entry[:etag]
       url = ghurl "repos/#{owner}/#{repo}"
       lu = last_updated(url, etag)
-      debug "Repo #{owner}/#{repo} last updated: #{lu}, db_entry: #{db_entry[:updated_at]}"
+      debug "Repo #{owner}/#{repo} last_modified: #{lu}, db_entry: #{db_entry[:updated_at]}"
 
-      if lu > db_entry[:updated_at]
+      if lu.to_i >= db_entry[:updated_at].to_i
         fresh_repo = retrieve_repo(owner, repo, true)
 
         unless fresh_repo.nil?
           db.from(:projects).
               where(:id => db_entry[:id]).
               update(:etag => fresh_repo['etag'],
-                     :updated_at => lu)
+                     :updated_at => date(now))
 
-          info "Repo #{owner}/#{repo} updated at #{lu} (etag: #{fresh_repo['etag']})"
+          info "Repo #{owner}/#{repo} updated #{now} (etag: #{fresh_repo['etag']}, last_modified: #{lu})"
         end
 
         return db[:projects].first(:id => db_entry[:id])
